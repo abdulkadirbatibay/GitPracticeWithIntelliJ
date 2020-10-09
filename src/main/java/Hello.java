0f6d9a4250cdd82fce09827a780650c2676ffee6
Akadir <abdulkadirbatibay@gmail.com>
2020-10-09 08:36:16 +0200

    EU3 DOING GREAT!

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,8 @@ public class Hello {
         //more changes are coming
         //I am ready to commit this
 
+        //EU3 IS AWESOME!!!
+        //EU3 IS LEARNING HOW TO USE GIT FROM INTELLIJ
+
     }
 }
