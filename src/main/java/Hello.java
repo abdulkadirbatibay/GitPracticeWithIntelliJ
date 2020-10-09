ae9dc40d189871c4a40902fd33fb2f41d785f4db
Akadir <abdulkadirbatibay@gmail.com>
2020-10-09 08:22:24 +0200

    I have updated the Hello.java file.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello class EU3");
+        //more changes are coming
+        //I am ready to commit this
 
     }
 }
